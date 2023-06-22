72aadc95c535872847334120f75e505068bf100a
qwerty59b <130035937+qwerty59b@users.noreply.github.com>
2023-06-22 18:15:40 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 if [[ -n $RCLONE_CONFIG ]]; then
  echo "Rclone config detected"
- echo -e "$RCLONE_CONFIG" > /app/rclone.conf
+ echo -e "$RCLONE_CONFIG" > rclone.conf
 fi
 chmod +x extract
 python3 -m tobrot
