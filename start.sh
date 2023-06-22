a852dd7491a5cf1d7903be4f27df03b8963aa403
qwerty59b <130035937+qwerty59b@users.noreply.github.com>
2023-06-22 16:54:18 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
-/etc/init.d/apache2 start
+if [[ -n $RCLONE_CONFIG ]]; then
+ echo "Rclone config detected"
+ echo -e "$RCLONE_CONFIG" > /app/rclone.conf
+fi
+chmod +x extract
 python3 -m tobrot
 
 
