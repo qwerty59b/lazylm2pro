06a94c5825a8e4237d39277feb30cd61b09177d5
qwerty59b <130035937+qwerty59b@users.noreply.github.com>
2023-06-22 23:04:43 -0500

    Update cloneHelper.py

--- tobrot/helper_funcs/cloneHelper.py
+++ tobrot/helper_funcs/cloneHelper.py
@@ -135,7 +135,7 @@ class CloneHelper:
             destination = f"{DESTINATION_FOLDER}"
             idd = "{" f"{self.g_id}" "}"
             cmd = [
-                "/app/gopal/gclone",
+                "gclone",
                 "copy",
                 "--config=rclone.conf",
                 f"{self.dname}:{idd}",
