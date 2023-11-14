f6d9e39469ec832bd7cf197305bb22185ae56266
qwerty59b <130035937+qwerty59b@users.noreply.github.com>
2023-11-14 17:11:10 -0500

    Update __init__.py

--- tobrot/__init__.py
+++ tobrot/__init__.py
@@ -67,7 +67,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1153151858"))
 
 # Get these values from my.telegram.org
 # to store the channel ID who are authorized to use the bot
-AUTH_CHANNEL = [int(x) for x in os.environ.get("AUTH_CHANNEL", "-1001918767797").split()]
+AUTH_CHANNEL = [int(x) for x in os.environ.get("AUTH_CHANNEL", "-1001931068266").split()]
 
 # the download location, where the HTTP Server runs
 DOWNLOAD_LOCATION = "./DOWNLOADS"
