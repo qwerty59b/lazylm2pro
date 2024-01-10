0fad106fcbda6b7773f4871fb92af7fb561dc37f
qwerty59b <130035937+qwerty59b@users.noreply.github.com>
2024-01-10 07:22:54 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
+if [[ -n $RCLONE_CONFIG_URL ]]; then
+  echo "Rclone config detected"
+  wget -q $RCLONE_CONFIG_URL -O rclone.conf
+fi
+
 if [[ -n $RCLONE_CONFIG ]]; then
  echo "Rclone config detected"
  echo -e "$RCLONE_CONFIG" > rclone.conf
